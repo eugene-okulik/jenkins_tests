8725590a28993073dc8138beb3b4842187e56bdb
eugeny.okulik <tiburon854@gmail.com>
2022-09-15 22:00:20 +0300

    added pylint config

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,4 +8,4 @@ class BasePage:
 
     def find_elements(self, *args):
         by_name, by_val = args[0]
-        return self.driver.find_element(by_name, by_name)
+        return self.driver.find_element(by_name, by_val)
